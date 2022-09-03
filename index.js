c7963d477ab8413c80b35f8fb5bc58b9610bd4f8
kartikafanya <63616080+kartikafanya@users.noreply.github.com>
2022-09-03 12:36:42 +0700

    Add project baru

---
+++ index.js
@@ -0,0 +1,31 @@
+const DATABASE_URL =
+  "postgres://jmhievtcwiomiv:520b213f3b0e6b08f468be243be18f3141ca5d65266a6ab1fc169a52eb58d1ef@ec2-34-227-120-79.compute-1.amazone-1.amazonaws.com:5432/dahemuarg642hk";
+
+const config = new URL(DATABASE_URL);
+
+const database = {
+  protocol: config.protocol,
+  username: config.username,
+  password: config.password,
+  path: config.pathname,
+};
+// console.log("protocol", config.protocol);
+// console.log("username", config.username);
+// console.log("password", config.password);
+// console.log("path", config.pathname);
+// console.log("hostname", config.hostname);
+
+// const express = require("express");
+// const app = express();
+// const { PORT = 8000 } = process.env;
+// const cors = require("cors");
+
+// app.use(cors());
+
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Express running on port ${PORT}`);
+// });
